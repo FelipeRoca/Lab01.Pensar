b3997cc0405a9625c8f4a2f41217dbb1b61f8878
FelipeRoca <feliroca01@gmail.com>
2023-04-27 19:25:46 -0300

    ejercicio 6 y 8, hehcos y probados

--- Lab01.Pensar/lab01.cs
+++ Lab01.Pensar/lab01.cs
@@ -73,58 +73,58 @@ namespace Lab01.Pensar
 
 
 
-            /* // EJERCICIO 5
+            // EJERCICIO 5
             string mes = Console.ReadLine();
 
             switch (mes)
             {
-                case "enero":
+                case (mes=="enero"):
 
-                    Console.WriteLine(mes+" 1");
+                    Console.WriteLine(mes+"1");
                     break;
 
-                case "febrero":
-                    Console.WriteLine(mes+" 2");
+                case (mes=="febrero"):
+                    Console.WriteLine(mes+"2");
                     break;
 
-                case "marzo":
-                    Console.WriteLine(mes + " 3");
+                case (mes == "marzo"):
+                    Console.WriteLine(mes + "3");
                     break;
 
-                case "abril":
-                    Console.WriteLine(mes + " 4");
+                case (mes == "abril"):
+                    Console.WriteLine(mes + "4");
                     break;
 
-                case  "mayo":
-                    Console.WriteLine(mes + " 5");
+                case (mes == "mayo"):
+                    Console.WriteLine(mes + "5");
                     break;
 
-                case "junio":
-                    Console.WriteLine(mes + " 6");
+                case (mes == "junio"):
+                    Console.WriteLine(mes + "6");
                     break;
 
-                case "julio":
-                    Console.WriteLine(mes + " 7");
+                case (mes == "julio"):
+                    Console.WriteLine(mes + "7");
                     break;
 
-                case "agosto":
-                    Console.WriteLine(mes + " 8");
+                case (mes == "agosto"):
+                    Console.WriteLine(mes + "8");
                     break;
 
-                case "septiembre":
-                    Console.WriteLine(mes + " 9");
+                case (mes == "septiembre"):
+                    Console.WriteLine(mes + "9");
                     break;
 
-                case "octubre":
-                    Console.WriteLine(mes + " 10");
+                case (mes == "octubre"):
+                    Console.WriteLine(mes + "10");
                     break;
 
-                case "noviembre":
-                    Console.WriteLine(mes + " 11");
+                case (mes == "noviembre"):
+                    Console.WriteLine(mes + "11");
                     break;
 
-                case "diciembre":
-                    Console.WriteLine(mes + " 12");
+                case (mes == "diciembre"):
+                    Console.WriteLine(mes + "12");
                     break;
 
 
@@ -137,68 +137,7 @@ namespace Lab01.Pensar
 
                 default:
                     Console.WriteLine("error");
-                    break;
             }
-            */
-
-
-
-
-
-            // EJERCICIO 6
-
-            //Se ve bastante complejho, no encontre una funcion q lo haga automaticamente
-
-
-
-            /* //EJERCICIO 7
-
-             int n = int.Parse(Console.ReadLine());
-
-
-             for(int i =0; i<= n; i++) {
-                 int cont = 0;
-                 for (int j = 1; j <= n; j++)
-                 {
-                     if (i % j == 0) cont++;
-
-                 }
-                 if (cont < 3) Console.WriteLine(i);
-
-             }
-
-
-
-             */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -206,9 +145,5 @@ namespace Lab01.Pensar
 
 
         }
-
     }
 }
-
-
-
